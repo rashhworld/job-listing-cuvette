e9527d851453f303ae09ca745635cab35cf46714
Rashh <ranjan.rashmi2016@outlook.com>
2024-08-07 19:11:58 +0530

    trial fix navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,12 +22,12 @@ function Navbar({ setUserdata }) {
           {userName ? (
             <>
               <Link to="/job/create" className="text-white font-medium">Add Job</Link>
-              <div className="hidden md:flex items-center gap-4">
+              {/* <div className="hidden md:flex items-center gap-4">
                 <Link to="/user/signin" className="text-white font-medium" onClick={() => localStorage.removeItem('authJwtToken')}>Logout</Link>
                 <span className="text-white font-medium">Hello! <span className="hidden md:inline-block">{userName.split(" ")[0]}</span></span>
                 <img className="w-10 h-10 rounded-full" src="https://rashhworld.github.io/assets/images/profile.webp" alt="profile image" />
-              </div>
-              <div className="block md:hidden">
+              </div> */}
+              <div className="">
                 <img className="w-10 h-10 rounded-full" id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar" src="https://rashhworld.github.io/assets/images/profile.webp" alt="profile image" />
                 <div id="dropdownAvatar" className="z-10 hidden bg-red-500 text-white divide-y divide-gray-100 rounded me-2">
                   <p className="px-4 py-2 font-medium"><span>Hello!</span><br />{userName}</p>
